1762099eb996b2e97f7c52404cd9ef5998f75e68
Andrii Ivanushko <andivanushko@gmail.com>
2020-11-18 21:53:02 +0200

    Added chromed and gecko drivers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# com.insider
+# To do
+Chrome driver version 87.0.4280.20
+Geckodriver version 0.28.0
---
+++ src/test/java/com/insider/ui/SimpleTest.java
@@ -0,0 +1,27 @@
+package com.insider.ui;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+
+public class SimpleTest {
+
+    @Test
+    public void firstTest() {
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://useinsider.com/");
+        driver.close();
+    }
+
+    @Test
+    public void secondTest() {
+        System.setProperty(GeckoDriverService.GECKO_DRIVER_EXE_PROPERTY, "src/test/resources/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://useinsider.com/");
+        driver.close();
+    }
+}
