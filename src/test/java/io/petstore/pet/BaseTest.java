60ea3d87d50686208cba58871e0afc2058c77dc4
Andrii Ivanushko <andivanushko@gmail.com>
2020-11-26 10:17:12 +0200

    Listener was added to CRUD tests

--- src/test/java/io/petstore/pet/BaseTest.java
+++ src/test/java/io/petstore/pet/BaseTest.java
@@ -1,12 +1,15 @@
 package io.petstore.pet;
 
+import com.insider.config.TestListener;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 
 import static com.insider.config.Configuration.getPetStoreUrl;
 import static io.restassured.RestAssured.given;
 
+@Listeners(TestListener.class)
 public abstract class BaseTest {
 
     @BeforeClass
